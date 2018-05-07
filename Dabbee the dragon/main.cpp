fe79945c2e10dfa2ab4cebea2933f6f0528a0f11
Unknown <maartenwn@gmail.com>
2018-05-07 12:38:09 +0200

    Changed name to Dabbee the dragon

---
+++ Dabbee the dragon/main.cpp
@@ -0,0 +1,49 @@
+#include <GL\freeglut.h>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+
+
+int width;
+int height;
+
+
+void keyboard(unsigned char key, int x, int y) {
+}
+
+void SpecialInput(int key, int x, int y) {
+}
+
+void idle() {
+}
+
+void display() {
+}
+
+void onReshape(int w, int h)
+{
+	width = w;
+	height = h;
+
+	glViewport(0, 0, width, height);
+}
+
+void onMouseMotion(int x, int y) {
+}
+
+int main(int argc, char* argv[]) {
+	glutInitDisplayMode(GLUT_RGB | GL_DEPTH_BUFFER_BIT);
+	glutInitWindowSize(1200, 900);
+	glutInit(&argc, argv);
+	glutCreateWindow("Dabbee the dragon");
+
+
+	glutReshapeFunc(onReshape);
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutKeyboardFunc(keyboard);
+	glutPassiveMotionFunc(onMouseMotion);
+	glutSpecialFunc(SpecialInput);
+
+	glutMainLoop();
+	return 0;
+}
